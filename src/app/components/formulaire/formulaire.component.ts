c0e807b1c26181834707ff0ee087a67571c4105a
su apt install kuma <maxxozou@gmail.com>
2024-01-22 16:05:08 +0100

    0.1.2

--- src/app/components/formulaire/formulaire.component.ts
+++ src/app/components/formulaire/formulaire.component.ts
@@ -3,22 +3,23 @@ import { FormsModule } from '@angular/forms';
 import { ListeTachesComponent } from '../liste-taches/liste-taches.component';
 
 @Component({
-  selector: 'app-formulaire',
-  standalone: true,
-  imports: [FormsModule, ListeTachesComponent],
-  templateUrl: './formulaire.component.html',
-  styleUrl: './formulaire.component.css'
+    selector: 'app-formulaire',
+    standalone: true,
+    imports: [FormsModule, ListeTachesComponent],
+    templateUrl: './formulaire.component.html',
+    styleUrl: './formulaire.component.css'
 })
 export class FormulaireComponent {
-  taches:any[] = [];
+    taches: any[] = [];
 
-  newTache:any = {
-      nomTache: "",
-      accompli: false
-  };
+    newTache: any = {
+        nomTache: "",
+        accompli: false
+    };
 
-  addTache():void {
-      this.taches.push(this.newTache);
-      this.newTache = {};
-  }
+    addTache(): void {
+        const newTask = { nomTache: this.newTache.nomTache, accompli: this.newTache.accompli || false };
+        this.taches.push(newTask);
+        this.newTache = { nomTache: "", accompli: false };
+    }
 }
--- src/app/components/liste-taches/liste-taches.component.ts
+++ src/app/components/liste-taches/liste-taches.component.ts
@@ -1,24 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-liste-taches',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './liste-taches.component.html',
-  styleUrl: './liste-taches.component.css'
+    selector: 'app-liste-taches',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './liste-taches.component.html',
+    styleUrl: './liste-taches.component.css'
 })
-export class ListeTachesComponent{
 
-  @Input()
-  recupTaches!:any;
+export class ListeTachesComponent implements OnInit{
+    @Input() recupTaches!: any[];
+    filteredTasks: any[] = [];
+    filtreTaches: string = "all";
 
-  changeValueTache(index:number):void {
-    this.recupTaches[index].accompli = !this.recupTaches[index].accompli;
-    console.log(this.recupTaches)
-  }
+    ngOnInit(): void {
+        this.onChangeFiltre();
+    }
 
-  deleteTache(index:number):void {
-    this.recupTaches.splice(index, 1);
-  }
+    changeValueTache(index: number): void {
+        this.recupTaches[index].accompli = !this.recupTaches[index].accompli;
+    }
+
+    deleteTache(index: number): void {
+        const originalIndex = this.recupTaches.indexOf(this.filteredTasks[index]);
+        this.recupTaches.splice(originalIndex, 1);
+        this.onChangeFiltre();
+    }
+
+    onChangeFiltre(): void {
+        if (this.filtreTaches === "accomplies") {
+            this.filteredTasks = this.recupTaches.filter(tache => tache.accompli);
+        } else if (this.filtreTaches === "nonAccomplies") {
+            this.filteredTasks = this.recupTaches.filter(tache => !tache.accompli);
+        } else {
+            this.filteredTasks = this.recupTaches.slice();
+        }
+    }
 }
